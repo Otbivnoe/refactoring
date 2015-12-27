ed534dcf83f89b76a634c5fc0483b410c1520a12
Nikita <nikita@Nikitas-Mini.Dlink>
2015-12-27 22:12:41 +0600

    Changed MapHandlersHelper class

--- Refactoring/src/main/java/com/nikita/refactoring/Guice/JsonHelper.java
+++ Refactoring/src/main/java/com/nikita/refactoring/Guice/JsonHelper.java
@@ -2,17 +2,15 @@ package com.nikita.refactoring.Guice;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.FileReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 /**
  * Created by nikita on 29/11/15.
  */
+
 public class JsonHelper {
 
     private static final String JSON_FILE_PATH = "/Users/nikita/Documents/Projects/Universe/TRPO/gitRefactoring/Refactoring/src/main/java/com/nikita/refactoring/Guice/ClassManager.json";
@@ -35,12 +33,8 @@ public class JsonHelper {
                 return object;
             }
             return null;
-        } catch (FileNotFoundException e) {
-            log.error("File not found", e);
-        } catch (ParseException e) {
-            log.error("Parse exception", e);
-        } catch (IOException e) {
-            log.error("IOException", e);
+        } catch (Exception e) {
+            log.error("Json exception", e);
         }
 
         return null;
--- Refactoring/src/main/java/com/nikita/refactoring/Handlers/MapHandlersHelper.java
+++ Refactoring/src/main/java/com/nikita/refactoring/Handlers/MapHandlersHelper.java
@@ -2,11 +2,15 @@ package com.nikita.refactoring.Handlers;
 
 import com.nikita.refactoring.Context;
 import com.nikita.refactoring.Guice.JsonHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by nikita on 27/12/15.
  */
 public class MapHandlersHelper {
+
+    private static final Logger log = LoggerFactory.getLogger(MapHandlersHelper.class);
 
     public static IHandler getCurrentHandlerForContext(Context context) {
 
@@ -14,10 +18,9 @@ public class MapHandlersHelper {
         String currentStage = context.getCurrentStage().toString();
         char currentSymbol = context.getCurrentCharacter();
 
-        String handlerPath;
-        if (currentSymbol == '{' || currentSymbol == '}' || currentSymbol == ';') {
-            handlerPath = currentLanguageMapHandlers + "." + currentSymbol + "." + currentStage;
-        } else {
+        String handlerPath = currentLanguageMapHandlers + "." + currentSymbol + "." + currentStage;
+        String handlerName = (String)JsonHelper.getObjectForKeyPath(handlerPath);
+        if (handlerName == null) {
             handlerPath = currentLanguageMapHandlers + "." + "others" + "." + currentStage;
         }
 
@@ -25,11 +28,11 @@ public class MapHandlersHelper {
             Class handlerClass = Class.forName((String)JsonHelper.getObjectForKeyPath(handlerPath));
             return (IHandler)handlerClass.newInstance();
         } catch (ClassNotFoundException e) {
-
+            log.error("ClassNotFoundException", e);
         } catch (InstantiationException e) {
-
+            log.error("InstantiationException", e);
         } catch (IllegalAccessException e) {
-
+            log.error("IllegalAccessException", e);
         }
         return null;
     }
